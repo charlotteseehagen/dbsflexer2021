ede2a1b007a1947fab149ea16a7ffe4f974acc96
seehagec01 <seehagec01@mi.fu-berlin.de>
2021-06-28 16:47:15 +0200

    foreign keys

--- project/postgres_sql/RICHTIGESSCHEMA.sql
+++ project/postgres_sql/RICHTIGESSCHEMA.sql
@@ -1,32 +1,40 @@
 --MEGA DATABASESCHEMA 420 69
 
+DROP TABLE IF EXISTS country;
+DROP TABLE IF EXISTS co2_emission;
+DROP TABLE IF EXISTS gdp;
+
+
 CREATE TABLE country (
+    country_id INTEGER,
 	country_code VARCHAR(3),
 	population_total FLOAT,
 	population_relative FLOAT,
     year INTEGER NOT NULL,
-    PRIMARY KEY (country_code)
+    PRIMARY KEY (country_id)
 );
 
 CREATE TABLE co2_emission (
+    co2_id INTEGER,
 	country_code_co2 VARCHAR(3),
 	tonnes FLOAT,
     year INTEGER NOT NULL,
-    PRIMARY KEY (country_code_co2),
+    PRIMARY KEY (co2_id),
     CONSTRAINT foreign_country_code_co2
-        FOREIGN KEY (country_code)
-        REFERENCES country(country_code)
+        FOREIGN KEY (country_id)
+        REFERENCES country(country_id)
 );
 
 CREATE TABLE gdp (
+    gdp_id INTEGER,
 	country_code_gdp VARCHAR(3),
 	expenses FLOAT,
     education_expenses FLOAT,
     military_expenses FLOAT,
-    PRIMARY KEY (country_code_gdp),
+    PRIMARY KEY (gdp_id),
 	CONSTRAINT foreign_country_code_gdp
-        FOREIGN KEY (country_code)
-        REFERENCES country(country_code)
+        FOREIGN KEY (country_id)
+        REFERENCES country(country_id)
 );
 
 
