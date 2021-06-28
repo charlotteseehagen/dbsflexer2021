d4d09ff13ab71968b82d8c8959c3c3b974600711
seehagec01 <seehagec01@mi.fu-berlin.de>
2021-06-28 13:19:25 +0200

    data type changes

--- project/dataschema.sql
+++ project/dataschema.sql
@@ -1,26 +1,26 @@
 
 CREATE TABLE country (
-	country_code INTEGER NOT NULL,
-	population_total INTEGER,
-	population_relative INTEGER,
+	country_code VARCHAR(3),
+	population_total FLOAT,
+	population_relative FLOAT,
     year INTEGER NOT NULL,
     PRIMARY KEY (country_code),
 	
 )
 
 CREATE TABLE co2_emission (
-	country_code_co2 INTEGER NOT NULL,
-	tonnes INTEGER,
+	country_code_co2 VARCHAR(3),
+	tonnes FLOAT,
     year INTEGER NOT NULL,
     PRIMARY KEY (country_code_co2),
 		
 )
 
 CREATE TABLE gdp (
-	country_code_gdp INTEGER NOT NULL,
-	expenses INTEGER,
-    education_expenses INTEGER,
-    military_expenses INTEGER,
+	country_code_gdp VARCHAR(3),
+	expenses FLOAT,
+    education_expenses FLOAT,
+    military_expenses FLOAT,
     PRIMARY KEY (country_code_gdp),
 	
 )
