37bf2b08fb1a1024daa60fc311918131e84befee
waldoh00 <helena.waldorf@fu-berlin.de>
2021-06-28 12:29:29 +0200

    änderungen rechtschreibung ups

--- project/dataschema.sql
+++ project/dataschema.sql
@@ -1,26 +1,26 @@
-CREATE TABLE CO2emission (
-	CountryCode(Co2) INTEGER NOT NULL,
+CREATE TABLE CO2_emission (
+	Country_Code_Co2 INTEGER NOT NULL,
 	Tonnes INTEGER,
       Year INTEGER NOT NULL,
-      PRIMARY KEY (CountryCode(Co2)), 
-	FOREIGN KEY (CountryCode) REFERENCES country(CountryCode)
+      PRIMARY KEY (Country_Code_Co2), 
+	FOREIGN KEY (Country_Code) REFERENCES country(Country_Code)
 );
 
 CREATE TABLE country (
-	CountryCode INTEGER NOT NULL,
+	Country_Code INTEGER NOT NULL,
 	Population_Total INTEGER,
 	Population_Relative INTEGER,
       Year INTEGER NOT NULL,
-      PRIMARY KEY (Country Code),
-	FOREIGN KEY (CountryCode(gdp)) REFERENCES gdp(CountryCode(gdp))
-	FOREIGN KEY (CountryCode(Co2)) REFERENCES CO2emission(CountryCode(Co2))
+      PRIMARY KEY (Country_Code),
+	FOREIGN KEY (Country_Code_gdp) REFERENCES gdp(CountryCode_gdp)
+	FOREIGN KEY (Country_Code_Co2) REFERENCES CO2_emission(Country_Code_Co2)
 );
 
 CREATE TABLE gdp (
-	CountryCode(gdp) INTEGER NOT NULL,
+	Country_Code_gdp INTEGER NOT NULL,
 	Expenses INTEGER,
       EducationExpenses INTEGER,
       MilitaryExpenses INTEGER,
-      PRIMARY KEY (CountryCode(gdp)),
-	FOREIGN KEY (CountryCode) REFERENCES country(CountryCode)
+      PRIMARY KEY (Country_Code_gdp),
+	FOREIGN KEY (Country_Code) REFERENCES country(Country_Code)
 );
