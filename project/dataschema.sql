19f82e953154b793fb9f61162b535bcebe58e9d9
waldoh00 <helena.waldorf@fu-berlin.de>
2021-06-28 12:36:53 +0200

    änderungen rechtschreibung ups

--- project/dataschema.sql
+++ project/dataschema.sql
@@ -1,10 +1,3 @@
-CREATE TABLE co2_emission (
-	country_code_co2 INTEGER NOT NULL,
-	tonnes INTEGER,
-      year INTEGER NOT NULL,
-      PRIMARY KEY (country_code_co2), 
-	FOREIGN KEY (country_code) REFERENCES country(country_code)
-);
 
 CREATE TABLE country (
 	country_code INTEGER NOT NULL,
@@ -14,6 +7,14 @@ CREATE TABLE country (
       PRIMARY KEY (country_code),
 	FOREIGN KEY (country_code_gdp) REFERENCES gdp(country_code_gdp)
 	FOREIGN KEY (country_code_co2) REFERENCES co2_emission(country_code_co2)
+);
+
+CREATE TABLE co2_emission (
+	country_code_co2 INTEGER NOT NULL,
+	tonnes INTEGER,
+      year INTEGER NOT NULL,
+      PRIMARY KEY (country_code_co2), 
+	FOREIGN KEY (country_code) REFERENCES country(country_code)
 );
 
 CREATE TABLE gdp (
