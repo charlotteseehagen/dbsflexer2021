0c04a9da43ec78c27b3cbc8c4c3248182629ac62
seehagec01 <seehagec01@mi.fu-berlin.de>
2021-06-28 12:52:50 +0200

    foreign keys

--- project/postgresql data/dataschema.sql
+++ project/postgresql data/dataschema.sql
@@ -1,26 +1,26 @@
-CREATE TABLE CO2_emission (
-	Country_Code_Co2 INTEGER NOT NULL,
-	Tonnes INTEGER,
-      Year INTEGER NOT NULL,
-      PRIMARY KEY (Country_Code_Co2), 
-	FOREIGN KEY (Country_Code) REFERENCES country(Country_Code)
-);
 
 CREATE TABLE country (
-	Country_Code INTEGER NOT NULL,
-	Population_Total INTEGER,
-	Population_Relative INTEGER,
-      Year INTEGER NOT NULL,
-      PRIMARY KEY (Country_Code),
-	FOREIGN KEY (Country_Code_gdp) REFERENCES gdp(CountryCode_gdp)
-	FOREIGN KEY (Country_Code_Co2) REFERENCES CO2_emission(Country_Code_Co2)
+	country_code INTEGER NOT NULL,
+	population_total INTEGER,
+	population_relative INTEGER,
+      year INTEGER NOT NULL,
+      PRIMARY KEY (country_code),
+	
+);
+
+CREATE TABLE co2_emission (
+	country_code_co2 INTEGER NOT NULL,
+	tonnes INTEGER,
+      year INTEGER NOT NULL,
+      PRIMARY KEY (country_code_co2), 
+		
 );
 
 CREATE TABLE gdp (
-	Country_Code_gdp INTEGER NOT NULL,
-	Expenses INTEGER,
-      EducationExpenses INTEGER,
-      MilitaryExpenses INTEGER,
-      PRIMARY KEY (Country_Code_gdp),
-	FOREIGN KEY (Country_Code) REFERENCES country(Country_Code)
+	country_code_gdp INTEGER NOT NULL,
+	expenses INTEGER,
+      education_expenses INTEGER,
+      military_expenses INTEGER,
+      PRIMARY KEY (country_code_gdp),
+	
 );
