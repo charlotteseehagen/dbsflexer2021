2a29e0dadede7e62d9393622d3204e6990725874
seehagec01 <seehagec01@mi.fu-berlin.de>
2021-06-30 15:57:58 +0200

    schema edited

--- project/postgres_sql/RICHTIGESSCHEMA.sql
+++ project/postgres_sql/RICHTIGESSCHEMA.sql
@@ -1,51 +1,26 @@
 --MEGA DATABASESCHEMA 420 69
 
 DROP TABLE IF EXISTS country;
-DROP TABLE IF EXISTS co2_emission;
-DROP TABLE IF EXISTS gdp;
+DROP TABLE IF EXISTS data;
 
 
 CREATE TABLE country (
-    country_id INT GENERATED ALWAYS AS IDENTITY,
-    co2_id INT,
-    gdp_id INT,
 	country_code VARCHAR(3),
-	population_total FLOAT,
-	population_relative FLOAT,
-    year INTEGER NOT NULL,
-    PRIMARY KEY (country_id)
+	country_name VARCHAR(255),
+    continent VARCHAR(25),
+    PRIMARY KEY (country_code)
 );
 
-CREATE TABLE co2_emission (
-    country_id INT,
-    co2_id INT GENERATED ALWAYS AS IDENTITY,
-	country_code_co2 VARCHAR(3),
-	tonnes FLOAT,
-    year INTEGER NOT NULL,
-    PRIMARY KEY (co2_id),
-    CONSTRAINT fk_country
-        FOREIGN KEY (country_id)
-            REFERENCES country(country_id)
+CREATE TABLE data (
+    country_code VARCHAR(3),
+    year INT,
+    gdp FLOAT,
+    co2_emission FLOAT,
+    expenses FLOAT,
+    population_total FLOAT,
+    population_relative FLOAT,
+    electricity_production_renewable FLOAT,
+    PRIMARY KEY (country_code, year)
 );
 
-CREATE TABLE gdp (
-    country_id INT,
-    gdp_id INT GENERATED ALWAYS AS IDENTITY,
-	country_code_gdp VARCHAR(3),
-	expenses FLOAT,
-    education_expenses FLOAT,
-    military_expenses FLOAT,
-    PRIMARY KEY (gdp_id),
-	CONSTRAINT fk_country
-        FOREIGN KEY (country_id)
-            REFERENCES country(country_id)
-);
-
-ALTER TABLE country
-    ADD CONSTRAINT fk_co2
-        FOREIGN KEY (co2_id)
-            REFERENCES co2_emission(co2_id),
-    ADD CONSTRAINT fk_gdp
-        FOREIGN KEY (gdp_id)
-            REFERENCES gdp(gdp_id)
         
