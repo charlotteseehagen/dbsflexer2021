4adc57c67d472de516bda5d23f4a1aa2e886ad42
seehagec01 <seehagec01@mi.fu-berlin.de>
2021-07-04 11:39:23 +0200

    query0

--- project/app.py
+++ project/app.py
@@ -7,16 +7,40 @@ import plotly.express as px
 import pandas as pd
 from db_conn.connector import connect 
 #QUERIES:
-query0 = "SELECT country_code, max(gdp) AS gdp\
-            FROM data\
-            WHERE year = 2000 AND gdp IS NOT NULL\
+query0 = "SELECT country_code,country_name, max(gdp) AS gdp\
+            FROM data, country\
+            WHERE year = 2000 AND gdp IS NOT NULL AND country_code == country_code \
             GROUP BY country_code, gdp\
+            ORDER BY gdp DESC\
+            LIMIT 10;"
+            
+query1 = "SELECT country_code, co2_emission, max(gdp) AS gdp \
+            FROM data \
+            WHERE country_code = 'USA' OR country_code = 'JPN' OR country_code = 'DEU' OR country_code = 'GBR' OR country_code = 'FRA' OR country_code = 'CHN' OR country_code = 'ITA' OR country_code = 'CAN' OR country_code = 'MEX' OR country_code = 'BRA'\
+            AND year > 1999 AND year < 2016\
+            GROUP BY country_code, co2_emission\
+            ORDER BY gdp DESC\
+            LIMIT 10;"
+                
+    query2 = "SELECT country_code, electricity_production_renewable, max(gdp) AS gdp\
+            FROM data\
+            WHERE WHERE country_code = 'USA' OR country_code = 'JPN' OR country_code = 'DEU' OR country_code = 'GBR' OR country_code = 'FRA' OR country_code = 'CHN' OR country_code = 'ITA' OR country_code = 'CAN' OR country_code = 'MEX' OR country_code = 'BRA'\
+            AND year > 1999 AND year < 2016\
+            GROUP BY country_code,  electricity_prodction\
+            ORDER BY gdp DESC\
+            LIMIT 10;"
+                
+    query3 = "SELECT country_code, co2_emission, electricity_production_renewable,                  max(gdp) AS gdp\
+            FROM data\
+            WHERE country_code = 'USA' OR country_code = 'JPN' OR country_code = 'DEU' OR country_code = 'GBR' OR country_code = 'FRA' OR country_code = 'CHN' OR country_code = 'ITA' OR country_code = 'CAN' OR country_code = 'MEX' OR country_code = 'BRA'\
+            AND year > 1999 AND year < 2016\
+            GROUP BY country_code, co2_emission, electricity_prodction\
             ORDER BY gdp DESC\
             LIMIT 10;"
 
 #establish connection and fetch queries
 def data_fetch():
-    raw_data = connect("SELECT * from data;")
+    raw_data = connect(query0)
     #transform data
     df0 = {"Country Code": [], "Year" : [], "GDP(USD)": []}
     
@@ -39,7 +63,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 #call data from db
 df = data_fetch()
 
-fig = px.scatter(df, x="Year", y="GDP(USD)", color="Country Code", hover_name="Country Code")
+fig = px.scatter(df, x="Year", y="GDP(USD)", color="Country Code", hover_name="Country Names")
 
 app.layout = html.Div(children=[
     html.H1(children='DBS-Project 2021'),
