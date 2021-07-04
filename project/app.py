43146b0813eb8af50680f2669f74c8769978be4f
Ubuntu <ubuntu@ip-172-31-33-43.us-east-2.compute.internal>
2021-07-04 12:08:18 +0000

    finishes plots

--- project/app.py
+++ project/app.py
@@ -6,103 +6,149 @@ import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 from db_conn.connector import connect 
-#QUERIES:
-query0 = "SELECT country_code,country_name, max(gdp) AS gdp\
-            FROM data, country\
-            WHERE year = 2000 AND gdp IS NOT NULL AND country_code == country_code \
-            GROUP BY country_code, gdp\
-            ORDER BY gdp DESC\
-            LIMIT 10;"
-            
-query1 = "SELECT country_code, co2_emission, max(gdp) AS gdp \
-            FROM data \
-            WHERE country_code = 'USA' OR country_code = 'JPN' OR country_code = 'DEU' OR country_code = 'GBR' OR country_code = 'FRA' OR country_code = 'CHN' OR country_code = 'ITA' OR country_code = 'CAN' OR country_code = 'MEX' OR country_code = 'BRA'\
-            AND year > 1999 AND year < 2016\
-            GROUP BY country_code, co2_emission\
-            ORDER BY gdp DESC\
-            LIMIT 10;"
-                
-query2 = "SELECT country_code, electricity_production_renewable, max(gdp) AS gdp\
-            FROM data\
-            WHERE WHERE country_code = 'USA' OR country_code = 'JPN' OR country_code = 'DEU' OR country_code = 'GBR' OR country_code = 'FRA' OR country_code = 'CHN' OR country_code = 'ITA' OR country_code = 'CAN' OR country_code = 'MEX' OR country_code = 'BRA'\
-            AND year > 1999 AND year < 2016\
-            GROUP BY country_code,  electricity_prodction\
-            ORDER BY gdp DESC\
-            LIMIT 10;"
-                
-query3 = "SELECT country_code, co2_emission, electricity_production_renewable,                  max(gdp) AS gdp\
-            FROM data\
-            WHERE country_code = 'USA' OR country_code = 'JPN' OR country_code = 'DEU' OR country_code = 'GBR' OR country_code = 'FRA' OR country_code = 'CHN' OR country_code = 'ITA' OR country_code = 'CAN' OR country_code = 'MEX' OR country_code = 'BRA'\
-            AND year > 1999 AND year < 2016\
-            GROUP BY country_code, co2_emission, electricity_prodction\
-            ORDER BY gdp DESC\
-            LIMIT 10;"
 
-#establish connection and fetch queries
-def data_fetch():
-    raw_data = connect(query0)
-    #transform data
-    df0 = {"Country Code": [], "Year" : [], "GDP(USD)": []}
-    
-    if raw_data[1]:
-        for tup in raw_data[0]:
-            df0["Country Code"].append(tup[0])
-            df0["Year"].append(tup[1])
-            df0["GDP(USD)"].append(tup[3])
+""" DIE MEGA COOLEN QUERIES
+    RATHER UGLY DESIGN BUT NO TIME
+    TIME IS PRECIOUS 
+"""
+query0 ="SELECT data.country_code, country_name, year, max(gdp) AS gdp \
+	FROM data, country \
+	WHERE year = 2000 AND gdp IS NOT NULL \
+	AND data.country_code = country.country_code\
+		GROUP BY data.country_code, country_name, year, gdp\
+		ORDER BY gdp DESC\
+		LIMIT 10;"
 
-    else:
-        print("FUCK")
+query1 ="SELECT foo.country_code, data.year, data.co2_emission, data.gdp, foo.country_name\
+	FROM (\
+	SELECT data.country_code,country_name, year, max(gdp) AS gdp\
+		FROM data, country\
+		WHERE year = 2000 AND gdp IS NOT NULL \
+		AND data.country_code = country.country_code\
+			GROUP BY data.country_code, country_name,year, gdp\
+			ORDER BY gdp DESC\
+			LIMIT 10\
+	) as foo, data, country \
+	WHERE foo.country_code = data.country_code AND data.year >= 2000\
+		GROUP BY foo.country_code, data.year, data.co2_emission, data.gdp, foo.country_name\
+		ORDER BY foo.country_code DESC,\
+			 data.year DESC;"
 
-    return pd.DataFrame(df0)
+query2 ="SELECT foo.country_code, data.year, data.electricity_production_renewable, data.gdp, foo.country_name\
+	FROM (\
+	SELECT data.country_code,country_name, year, max(gdp) AS gdp\
+		FROM data, country\
+		WHERE year = 2000 AND gdp IS NOT NULL \
+		AND data.country_code = country.country_code\
+			GROUP BY data.country_code, country_name,year, gdp\
+			ORDER BY gdp DESC\
+			LIMIT 10\
+	) as foo, data, country \
+	WHERE foo.country_code = data.country_code AND data.year >= 2000\
+		GROUP BY foo.country_code, data.year, data.co2_emission, data.gdp, foo.country_name,data.electricity_production_renewable\
+		ORDER BY foo.country_code DESC,\
+			 data.year DESC;"\
 
 
+
+""" VISU STUFF """
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-#call data from db
-df = data_fetch()
+#establish connection and fetch queries
+raw_data = connect(query0)
+#first figure bar --------------------------------------------------
+df0 = {"Country Code": [], "Country Name" : [], "Year" : [], "GDP(USD)" : []}
+#transform data
+if raw_data[1]:
+    for tup in raw_data[0]:
+        df0["Country Code"].append(tup[0])
+        df0["Country Name"].append(tup[1])
+        df0["Year"].append(tup[2])
+        df0["GDP(USD)"].append(tup[3])
 
-fig = px.scatter(df, x="Year", y="GDP(USD)", color="Country Code", hover_name="Country Names")
+else:
+    print("FUCK")
+
+pd.DataFrame(df0)
+fig = px.bar(df0, x="Country Code", y="GDP(USD)", color="Country Code", hover_name="Country Name")
+
+#first line graph --------------------------------------------------
+raw_data = connect(query1)
+df1 = {"Country Code": [], "Country Name" : [], "Year" : [], "GDP(USD)" : [], "Co2 Emissions(tonnes)" : []}
+if raw_data[1]:
+    for tup in raw_data[0]:
+        df1["Country Code"].append(tup[0])
+        df1["Year"].append(tup[1])
+        df1["Co2 Emissions(tonnes)"].append(tup[2])
+        df1["GDP(USD)"].append(tup[3])
+        df1["Country Name"].append(tup[4])
+
+else:
+    print("FUCK")
+
+pd.DataFrame(df1)
+fig0 = px.line(df1, x="Year", y="Co2 Emissions(tonnes)", color="Country Code", hover_name="Country Name")
+
+#second line graph -------------------------------------------------
+raw_data = connect(query2)
+df2 = {"Country Code": [], "Country Name" : [], "Year" : [], "GDP(USD)" : [], "Renewable Electricity Sources(Kwh)" : []}
+if raw_data[1]:
+    for tup in raw_data[0]:
+        df2["Country Code"].append(tup[0])
+        df2["Year"].append(tup[1])
+        df2["Renewable Electricity Sources(Kwh)"].append(tup[2])
+        df2["GDP(USD)"].append(tup[3])
+        df2["Country Name"].append(tup[4])
+
+else:
+    print("FUCK")
+
+pd.DataFrame(df2)
+fig2 = px.line(df2, x="Year", y="Renewable Electricity Sources(Kwh)" , color="Country Code", hover_name="Country Name")
 
 app.layout = html.Div(children=[
-    html.H1(children='DBS-Project 2021'),
+    # All elements from the top of the page
+    html.Div([
+        html.H1(children='Hello Dash'),
 
-    html.Div(children='''
-       Helena Waldorf, Charlotte Seehagen, Lenny Kovac 
-    '''),
+        html.Div(children='''
+            Dash: A web application framework for Python.
+        '''),
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+        dcc.Graph(
+            id='graph1',
+            figure=fig
+        ),
+    ]),
+    # New Div for all elements in the new 'row' of the page
+    html.Div([
+        html.H1(children='Hello Dash'),
+
+        html.Div(children='''
+            Dash: A web application framework for Python.
+        '''),
+
+        dcc.Graph(
+            id='graph2',
+            figure=fig0
+        ),
+    ]),
+    html.Div([
+        html.H1(children='Hello Dash'),
+
+        html.Div(children='''
+            Dash: A web application framework for Python.
+        '''),
+
+        dcc.Graph(
+            id='graph3',
+            figure=fig2
+        ),
+    ]),
 ])
 
 if __name__ == '__main__':
 
-                
-    query1 = "SELECT country_code, co2_emission, max(gdp) AS gdp \
-                FROM data \
-                WHERE country_code = 'USA' OR country_code = 'JPN' OR country_code = 'DEU' OR country_code = 'GBR' OR country_code = 'FRA' OR country_code = 'CHN' OR country_code = 'ITA' OR country_code = 'CAN' OR country_code = 'MEX' OR country_code = 'BRA'\
-                AND year > 1999 AND year < 2016\
-                GROUP BY country_code, co2_emission\
-                ORDER BY gdp DESC\
-                LIMIT 10;"
-                
-    query2 = "SELECT country_code, electricity_production_renewable, max(gdp) AS gdp\
-                FROM data\
-                WHERE WHERE country_code = 'USA' OR country_code = 'JPN' OR country_code = 'DEU' OR country_code = 'GBR' OR country_code = 'FRA' OR country_code = 'CHN' OR country_code = 'ITA' OR country_code = 'CAN' OR country_code = 'MEX' OR country_code = 'BRA'\
-                AND year > 1999 AND year < 2016\
-                GROUP BY country_code,  electricity_prodction\
-                ORDER BY gdp DESC\
-                LIMIT 10;"
-                
-    query3 = "SELECT country_code, co2_emission, electricity_production_renewable,                  max(gdp) AS gdp\
-                FROM data\
-                WHERE WHERE country_code = 'USA' OR country_code = 'JPN' OR country_code = 'DEU' OR country_code = 'GBR' OR country_code = 'FRA' OR country_code = 'CHN' OR country_code = 'ITA' OR country_code = 'CAN' OR country_code = 'MEX' OR country_code = 'BRA'\
-                AND year > 1999 AND year < 2016\
-                GROUP BY country_code, co2_emission, electricity_prodction\
-                ORDER BY gdp DESC\
-                LIMIT 10;"
-                
     app.run_server(debug=True,port=80,host='0.0.0.0')
+
